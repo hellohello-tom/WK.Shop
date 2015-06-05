7f97b4247a767bcf9ff91b3cd5bea3f93e054cda
halowangquan <505666221@qq.com>
2015-06-05 13:53:54 +0800

    类库命名空间变更

--- code/Shop.DAL/SysAdmin.cs
+++ code/Shop.DAL/SysAdmin.cs
@@ -20,111 +20,111 @@ namespace Shop.DAL
 	/// </summary>
 	public partial class SysAdminDAL : DALBase<SysAdmin>
 	{
-        /// <summary>
-        /// 增加一条数据 同时添加关系表
-        /// </summary>
-        /// <returns>返回自增id</returns>
-        public int Add(SysAdmin model)
-        {
-            int id = 0;
-            var trans = DB.BeginTrans();
-            try
-            {
-                //添加用户表
-                InsertCreator ic = InsertCreator.NewCreator().SetEntity<SysAdmin>(model);
-                trans.Excute(ic, out id);
-                //添加组织机构用户关系表
-                var orgUserModel = new SysOrgUser();
-                orgUserModel.OrgId = model.Org.Id;
-                orgUserModel.UserId = id;
-                orgUserModel.Detach();
-                trans.Save(orgUserModel);
-                trans.Commit();
-            }
-            catch (Exception ex)
-            {
-                id = 0;
-                Log.Logger.Error(ex);
-                trans.Rollback();
-            }
+		/// <summary>
+		/// 增加一条数据 同时添加关系表
+		/// </summary>
+		/// <returns>返回自增id</returns>
+		public int Add(SysAdmin model)
+		{
+			int id = 0;
+			var trans = DB.BeginTrans();
+			try
+			{
+				//添加用户表
+				InsertCreator ic = InsertCreator.NewCreator().SetEntity<SysAdmin>(model);
+				trans.Excute(ic, out id);
+				//添加组织机构用户关系表
+				var orgUserModel = new SysOrgUser();
+				orgUserModel.OrgId = model.Org.Id;
+				orgUserModel.UserId = id;
+				orgUserModel.Detach();
+				trans.Save(orgUserModel);
+				trans.Commit();
+			}
+			catch (Exception ex)
+			{
+				id = 0;
+				Log.Logger.Error(ex);
+				trans.Rollback();
+			}
 
-            return id;
-        }
+			return id;
+		}
 
-        /// <summary>
-        /// 更新一条数据 同时更新关系表
-        /// </summary>	
-        public bool UpdateAdmin(SysAdmin model)
-        {
-            var isOk = false;
-            var trans = DB.BeginTrans();
-            try
-            {
-                //更新用户表
-                model.Attach();
-                trans.Save(model);
-                //更新 关系表
-                trans.Update<SysOrgUser>(SysOrgUser._.OrgId, model.Org.Id, SysOrgUser._.UserId == model.Id);
+		/// <summary>
+		/// 更新一条数据 同时更新关系表
+		/// </summary>	
+		public bool UpdateAdmin(SysAdmin model)
+		{
+			var isOk = false;
+			var trans = DB.BeginTrans();
+			try
+			{
+				//更新用户表
+				model.Attach();
+				trans.Save(model);
+				//更新 关系表
+				trans.Update<SysOrgUser>(SysOrgUser._.OrgId, model.Org.Id, SysOrgUser._.UserId == model.Id);
 
-                trans.Commit();
-                isOk = true;
-            }
-            catch (Exception ex)
-            {
-                isOk = false;
-                Log.Logger.Error(ex);
-                trans.Rollback();
-            }
-            return isOk;
-        }
+				trans.Commit();
+				isOk = true;
+			}
+			catch (Exception ex)
+			{
+				isOk = false;
+				Log.Logger.Error(ex);
+				trans.Rollback();
+			}
+			return isOk;
+		}
 
 
-        /// <summary>
-        /// 删除一条数据
-        /// </summary>
-        public bool Delete(int id)
-        {
-            var isOk = false;
-            var trans = DB.BeginTrans();
-            try
-            {
-                trans.Delete<SysOrgUser>(SysOrgUser._.UserId == id);
-                trans.Delete<SysAdmin>(SysAdmin._.Id == id);
-                trans.Commit();
-                isOk = true;
-            }
-            catch (Exception ex)
-            {
-                isOk = false;
-                Log.Logger.Error(ex);
-                trans.Rollback();
-            }
-            return isOk;
-        }
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(int id)
+		{
+			var isOk = false;
+			var trans = DB.BeginTrans();
+			try
+			{
+				trans.Delete<SysOrgUser>(SysOrgUser._.UserId == id);
+				trans.Delete<SysAdmin>(SysAdmin._.Id == id);
+				trans.Commit();
+				isOk = true;
+			}
+			catch (Exception ex)
+			{
+				isOk = false;
+				Log.Logger.Error(ex);
+				trans.Rollback();
+			}
+			return isOk;
+		}
 
-        /// <summary>
-        /// 删除多条数据
-        /// </summary>
-        /// <param name="ids"></param>
-        /// <returns></returns>
-        public int Delete(int[] ids)
-        {
-            var count = 0;
-            var trans = DB.BeginTrans();
-            try
-            {
-                
-                trans.Delete<SysOrgUser>(SysOrgUser._.Id.In(ids));
-                count = trans.Delete<SysAdmin>(SysAdmin._.Id.In(ids));
-                trans.Commit();
-            }
-            catch (Exception ex)
-            {
-                count = 0;
-                Log.Logger.Error(ex);
-                trans.Rollback();
-            }
-            return count;
-        }
+		/// <summary>
+		/// 删除多条数据
+		/// </summary>
+		/// <param name="ids"></param>
+		/// <returns></returns>
+		public int Delete(int[] ids)
+		{
+			var count = 0;
+			var trans = DB.BeginTrans();
+			try
+			{
+				
+				trans.Delete<SysOrgUser>(SysOrgUser._.Id.In(ids));
+				count = trans.Delete<SysAdmin>(SysAdmin._.Id.In(ids));
+				trans.Commit();
+			}
+			catch (Exception ex)
+			{
+				count = 0;
+				Log.Logger.Error(ex);
+				trans.Rollback();
+			}
+			return count;
+		}
 	}
 }
