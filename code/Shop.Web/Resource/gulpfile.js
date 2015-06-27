8a20d81b08d75d395cf9a7162a4e4f96f735de08
Nexus <satanzlb@gmail.com>
2015-06-27 22:29:41 +0800

    *)修改构建工具文件集gulpfile.js,package.json

--- code/Shop.Web/Resource/gulpfile.js
+++ code/Shop.Web/Resource/gulpfile.js
@@ -1,59 +1,68 @@
 var gulp = require('gulp'),
-   jshint=require('gulp-jshint'),
-   uglify = require('gulp-uglify'),
-   minifyCss=require('gulp-minify-css'),
-   rename=require('gulp-rename'),
-   concat=require('gulp-concat'),
-   del=require('del'),
-   browserSync=require('browser-sync');
+    browserSync=require('browser-sync'),
+    concat=require('gulp-concat'),
+    coffee=require('gulp-coffee'),
+    del=require('del'),
+    gutil=require('gulp-util'),
+    jshint=require('gulp-jshint'),
+    minifyCss=require('gulp-minify-css'),
+    rename=require('gulp-rename'),
+    runsequence=require('run-sequence'),
+    sourcemaps=require("gulp-sourcemaps"),
+    uglify = require('gulp-uglify');
 
 gulp.task('clean',function(cb){
 	del(['build/css','build/scripts'],cb)
-})
+});
 
 gulp.task('build',['minify','minifycss'],function(){
-   console.log("Good Job.")
-})
+   console.log("Good Job!")
+});
 
-gulp.task("default",['browser-sync'],function(){
+gulp.task("default",['browser-sync','coffee'],function(){
 	// gulp.start('minify','minifycss')
    console.log("Enjoy!")
-})
+});
 
 gulp.task('minify', function () {
-   gulp.src('scripts/*.js')
+   gulp.src('./scripts/*.js')
+      .pipe(sourcemaps.init())
       .pipe(uglify())
       .pipe(rename({suffix:'.min'}))
-      .pipe(gulp.dest('build/scripts'));
+      .pipe(sourcemaps.write('../maps'))
+      .pipe(gulp.dest('./scripts'));
 });
 
 gulp.task('concat',function(){
-   gulp.src('scripts/*.js')
+   gulp.src('./scripts/*.js')
    .pipe(uglify())
    .pipe(concat('site.js'))
-   .pipe(gulp.dest('build/scripts'))
-})
+   .pipe(gulp.dest('./build/scripts'))
+});
 
 gulp.task("greet",function(){
 	console.log('Hello World!');
-})
+});
 
 gulp.task('jshint',function(){
-	gulp.src('js/*.js').pipe(jshint());
-})
-gulp.task("minifycss",function(){
-	gulp.src("css/*.css").pipe(minifyCss()).pipe(rename({suffix:'.min'})).pipe(gulp.dest('build/css'))
-})
+	gulp.src('./scripts/*.js').pipe(jshint());
+});
 
-gulp.task("jshint",function(){
-   gulp.src("scripts/*.js").pipe(jshint())
-})
+gulp.task("minifycss",function(){
+	gulp.src("./css/*.css").pipe(minifyCss()).pipe(rename({suffix:'.min'})).pipe(gulp.dest('./css/'))
+});
+
+gulp.task('coffee',function(){
+   gulp.src('coffee/*.coffee')
+   .pipe(sourcemaps.init())
+   .pipe(coffee({bare:true}).on('error',gutil.log))
+   .pipe(sourcemaps.write('../maps'))
+   .pipe(gulp.dest('./scripts'))
+});
 
 gulp.task('watch',function(){
-   gulp.watch("**",function(event){
-      console.log('Event type:'+event.type);
-   })
-})
+   gulp.watch("./coffee/**.coffee",['coffee'])
+});
 
 gulp.task("browser-sync",function(){
    browserSync({
@@ -63,4 +72,4 @@ gulp.task("browser-sync",function(){
       }
       // ,port:2015
    });
-})
+});
